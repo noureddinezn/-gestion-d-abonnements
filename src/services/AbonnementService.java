4b71fcd88f29a5f2df4f1522db4129cf1828667f
noureddinezn <noureddinezouana@gmail.com>
2026-09-18 16:25:49 +0100

    feat(subscription-service): import subscription domain types

--- src/services/AbonnementService.java
+++ src/services/AbonnementService.java
@@ -2,6 +2,10 @@ package services;
 
 import dao.AbonnementDAO;
 import dao.PaiementDAO;
+import model.Abonnement;
+import model.Paiement;
+import model.StatutAbonnement;
+import model.StatutPaiement;
 
 public class AbonnementService {
 }
