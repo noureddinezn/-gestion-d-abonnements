c32594f38607e0a2309edb5af0eb314ea3ac3822
noureddinezn <noureddinezouana@gmail.com>
2026-09-18 16:25:51 +0100

    feat(payment-service): import payment and date types

--- src/services/PaiementService.java
+++ src/services/PaiementService.java
@@ -2,6 +2,12 @@ package services;
 
 import dao.AbonnementDAO;
 import dao.PaiementDAO;
+import model.Abonnement;
+import model.Paiement;
+import model.StatutPaiement;
+import java.time.LocalDate;
+import java.util.List;
+
 
     private PaiementDAO paiementDAO;
 }
