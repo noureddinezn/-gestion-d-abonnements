d0708a2828c5317e9814bd008be90b6fa5c146ed
noureddinezn <noureddinezouana@gmail.com>
2026-09-18 11:09:40 +0100

    chore: add Java runtime entry point

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println("Version de Java exécutée : " + version);
+
+        if (version.startsWith("1.8")) {
+            System.out.println("-> Vous utilisez bien Java 8 !");
+        } else {
+            System.out.println("-> Attention : Vous n'utilisez pas Java 8.");
+        }
+    }
+}
